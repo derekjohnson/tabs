c72edd12d1ad845a44da01ee51e79a590cf06ebc
Derek Johnson <derekmj74@gmail.com>
2014-04-01 12:56:04 +0100

    very basic boilerplate and alpha JS

--- tabs.js
+++ tabs.js
@@ -1,45 +1,164 @@
 (function(win, doc, undefined) {
 	'use strict';
 
-	/* ==========================================================================
-	   Helper functions
-	   ========================================================================== */
-
-	// Cross browser events
-	var add_event = function(el, ev, fn) {
-		'addEventListener' in win ? 
-			return el.addEventListener(ev, fn, false) : 
-			return el.attachEvent('on' + ev, fn);
-	};
-
-	// Faster class selectors
-	var single_class = function(className) {
-		'getElementsByClassName' in doc ? 
-			return doc.getElementsByClassName(className)[0] : 
-			return doc.querySelector('.' + className);
-	}
-
-	var many_classes = function(className) {
-		'getElementsByClassName' in doc ? 
-			return doc.getElementsByClassName(className) : 
-			return doc.querySelectorAll('.' + className);
-	}
-
-	/* ==========================================================================
-	   
-	   ========================================================================== */
+	// Quick feature test
 	if(!'querySelector' in doc) {
-		// No point in going any further
+
 		return;
+
 	} else {
-		var tabs = function(config) {
 
+		var tabs = function() {
+
+			/* Helper functions
+			   ========================================================================== */
+
+			// Cross browser events
+			var add_event = function(el, ev, fn) {
+				'addEventListener' in win ? 
+					el.addEventListener(ev, fn, false) : 
+					el.attachEvent('on' + ev, fn);
+			};
+
+			// Faster class selectors
+			var get_single_by_class = function(className) {
+				return 'getElementsByClassName' in doc ? 
+					doc.getElementsByClassName(className)[0] : 
+					doc.querySelector('.' + className);
+			}
+
+			var get_many_by_class = function(className) {
+				return 'getElementsByClassName' in doc ? 
+					doc.getElementsByClassName(className) : 
+					doc.querySelectorAll('.' + className);
+			}
+
+
+			/* Feature detect for localStorage courtesy of 
+			   http://mathiasbynens.be/notes/localstorage-pattern
+			   ========================================================================== */
+			var storage,
+				fail,
+				uid;
+
+			try {
+				uid = new Date;
+				(storage = win.localStorage).setItem(uid, uid);
+				fail = storage.getItem(uid) != uid;
+				storage.removeItem(uid);
+				fail && (storage = false);
+			} catch(e) {}
+
+
+			/* DOM nodes we'll need
+			   ========================================================================== */
+
+			var wrapper = get_single_by_class('js-tab-ui'),
+				panels = get_many_by_class('js-panel'),
+				tab_names = get_many_by_class('js-panel__title'),
+				i,
+				ii = panels.length;
+
+
+			/* Show/hide the panels, update the tabs
+			   ========================================================================== */
+
+			var show_hide = function(event) {
+				var x,
+					x_id;
+
+				typeof event.target !== 'undefined' ?
+					x = event.target :
+					x = event.srcElement;
+
+				if(x.nodeName.toLowerCase() === 'li') {
+					// get the id of the clicked tab
+					x_id = x.id;
+				}
+
+				for(i=0; i<ii; i++) {
+					// display the correct panel, hide the others
+					if(panels[i].getAttribute('aria-labelledby') === x_id) {
+						panels[i].style.display = 'block';
+					} else {
+						panels[i].style.display = 'none';
+					}
+
+					// update the ARIA
+					if(els[i].id === x_id) {
+						els[i].setAttribute('aria-selected', 'true');
+					} else {
+						els[i].setAttribute('aria-selected', 'false');
+					}
+				}
+			};
+
+
+			/* Create each tab item
+			   ========================================================================== */
+
+			var build_tab = function(el, text, classification) {
+				el.textContent = text;
+				el.className = classification;
+				el.setAttribute('role', 'tab');
+
+				return el;
+			};
+
+
+			/* Make an empty list that will hold the tabs
+			   ========================================================================== */
+
+			var frag = doc.createDocumentFragment(),
+				tabs = doc.createElement('ul');
+
+			// Basic attributes for the list
+			tabs.className = 'product-tabs';
+			tabs.setAttribute('role', 'tablist');
+
+
+			/* Build each tab and add all required attributes to tabs & panels
+			   ========================================================================== */
+
+			var els = [];
+
+			for(i=0; i<ii; i++) {
+				var li = build_tab(doc.createElement('li'), tab_names[i].textContent, 'product-tabs__item');
+
+				// Add unique attributes to each list item
+				li.id = 'tab' + (i + 1);
+				li.setAttribute('aria-controls', panels[i].id);
+
+				i === 0 ?
+					li.setAttribute('aria-selected', 'true') :
+					li.setAttribute('aria-selected', 'false');
+
+				// Stick them into the document fragment
+				frag.appendChild(li);
+
+				// Stick them into the items array
+				els[i] = li;
+
+				// Panels
+				panels[i].setAttribute('role', 'tabpanel');
+				panels[i].setAttribute('aria-labelledby', 'tab' + (i + 1));
+			}
+
+
+			/* Insert the tabs into the DOM
+			   ========================================================================== */
+
+			tabs.appendChild(frag);
+
+			wrapper.insertBefore(tabs, get_single_by_class('js-panel'));
+
+
+			/* Listen for clicks on the tab list
+			   ========================================================================== */
+			add_event(tabs, 'click', show_hide);
 		};
 
-		var config = {
-			panel_class: 'js-tab'
-		};
+		tabs();
 
-		tabs(config);
 	}
 })(this, this.document);
